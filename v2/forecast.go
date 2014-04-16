7b4ce0d98b77e84b0afd26eb83f3e9f435caf9e3
Mark Lussier <mlussier@gmail.com>
2014-04-16 10:40:14 -0700

    add json keys to structs for re-encoding

--- v2/forecast.go
+++ v2/forecast.go
@@ -14,68 +14,68 @@ const (
 )
 
 type Flags struct {
-	DarkSkyUnavailable string
-	DarkSkyStations    []string
-	DataPointStations  []string
-	ISDStations        []string
-	LAMPStations       []string
-	METARStations      []string
-	METNOLicense       string
-	Sources            []string
-	Units              string
+	DarkSkyUnavailable string   `json:""`
+	DarkSkyStations    []string `json:"darksky-stations"`
+	DataPointStations  []string `json:"datapoint-stations"`
+	ISDStations        []string `json:"isds-stations"`
+	LAMPStations       []string `json:"lamp-stations"`
+	METARStations      []string `json:"metars-stations"`
+	METNOLicense       string   `json:"metnol-license"`
+	Sources            []string `json:"sources"`
+	Units              string   `json:"units"`
 }
 
 type DataPoint struct {
-	Time                   float64
-	Summary                string
-	Icon                   string
-	SunriseTime            float64
-	SunsetTime             float64
-	PrecipIntensity        float64
-	PrecipIntensityMax     float64
-	PrecipIntensityMaxTime float64
-	PrecipProbability      float64
-	PrecipType             string
-	PrecipAccumulation     float64
-	Temperature            float64
-	TemperatureMin         float64
-	TemperatureMinTime     float64
-	TemperatureMax         float64
-	TemperatureMaxTime     float64
-	DewPoint               float64
-	WindSpeed              float64
-	WindBearing            float64
-	CloudCover             float64
-	Humidity               float64
-	Pressure               float64
-	Visibility             float64
-	Ozone                  float64
+	Time                   float64 `json:"time"`
+	Summary                string  `json:"summary"`
+	Icon                   string  `json:"icon"`
+	SunriseTime            float64 `json:"sunriseTime"`
+	SunsetTime             float64 `json:"sunsetTime"`
+	PrecipIntensity        float64 `json:"precipIntensity"`
+	PrecipIntensityMax     float64 `json:"precipIntensityMax"`
+	PrecipIntensityMaxTime float64 `json:"precipIntensityMaxTime"`
+	PrecipProbability      float64 `json:"precipProbability"`
+	PrecipType             string  `json:"precipType"`
+	PrecipAccumulation     float64 `json:"precipAccumulation"`
+	Temperature            float64 `json:"temperature"`
+	TemperatureMin         float64 `json:"temperatureMin"`
+	TemperatureMinTime     float64 `json:"temperatureMinTime"`
+	TemperatureMax         float64 `json:"temperatureMax"`
+	TemperatureMaxTime     float64 `json:"temperatureMaxTime"`
+	DewPoint               float64 `json:"dewPoint"`
+	WindSpeed              float64 `json:"windSpeed"`
+	WindBearing            float64 `json:"windBearing"`
+	CloudCover             float64 `json:"cloudCover"`
+	Humidity               float64 `json:"humidity"`
+	Pressure               float64 `json:"pressure"`
+	Visibility             float64 `json:"visibility"`
+	Ozone                  float64 `json:"ozone"`
 }
 
 type DataBlock struct {
-	Summary string
-	Icon    string
-	Data    []DataPoint
+	Summary string      `json:"summary"`
+	Icon    string      `json:"icon"`
+	Data    []DataPoint `json:"data"`
 }
 
 type alert struct {
-	Title   string
-	Expires float64
-	URI     string
+	Title   string  `json:"title"`
+	Expires float64 `json:"expires"`
+	URI     string  `json:"uri"`
 }
 
 type Forecast struct {
-	Latitude  float64
-	Longitude float64
-	Timezone  string
-	Offset    float64
-	Currently DataPoint
-	Minutely  DataBlock
-	Hourly    DataBlock
-	Daily     DataBlock
-	Alerts    []alert
-	Flags     Flags
-	APICalls  int
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	Timezone  string    `json:"timezone"`
+	Offset    float64   `json:"offset"`
+	Currently DataPoint `json:"currently"`
+	Minutely  DataBlock `json:"Minutely"`
+	Hourly    DataBlock `json:"hourly"`
+	Daily     DataBlock `json:"daily"`
+	Alerts    []alert   `json:"alerts"`
+	Flags     Flags     `json:"flags"`
+	APICalls  int       `json:"apicalls"`
 }
 
 type Units string
